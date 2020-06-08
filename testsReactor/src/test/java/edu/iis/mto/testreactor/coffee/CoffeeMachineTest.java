94683cac3ed64d61e328a280d17c982c2e36c833
Tomasz.Kucharczyk <215786@edu.p.lodz.pl>
2020-06-08 17:41:34 +0200

    CoffeMachineTest initialization

--- testsReactor/src/test/java/edu/iis/mto/testreactor/coffee/CoffeeMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/coffee/CoffeeMachineTest.java
@@ -1,15 +1,38 @@
 package edu.iis.mto.testreactor.coffee;
 
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
-import org.hamcrest.Matchers;
+
+import edu.iis.mto.testreactor.coffee.milkprovider.MilkProvider;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-class CoffeeMachineTest {
+@ExtendWith(MockitoExtension.class)
+public class CoffeeMachineTest {
+
+    @Mock
+    private Grinder grinder;
+    @Mock
+    private MilkProvider milkProvider;
+    @Mock
+    private CoffeeReceipes receipes;
+
+    private CoffeeMachine coffeeMachine;
+
+    @BeforeEach
+    void init() {
+        coffeeMachine = new CoffeeMachine(grinder, milkProvider, receipes);
+    }
+
+    
 
     @Test
     public void itCompiles() {
-        assertThat(true, Matchers.equalTo(true));
+        assertThat(true, equalTo(true));
     }
 
 }
