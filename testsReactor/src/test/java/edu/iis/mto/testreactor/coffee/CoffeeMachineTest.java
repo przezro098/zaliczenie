885e10342cf794e612108312b83a620dd0a0fe20
Tomasz.Kucharczyk <215786@edu.p.lodz.pl>
2020-06-08 18:16:51 +0200

    First state test added

--- testsReactor/src/test/java/edu/iis/mto/testreactor/coffee/CoffeeMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/coffee/CoffeeMachineTest.java
@@ -1,38 +1,51 @@
 package edu.iis.mto.testreactor.coffee;
 
-import static org.hamcrest.Matchers.equalTo;
-
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import edu.iis.mto.testreactor.coffee.milkprovider.MilkProvider;
 
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith(MockitoExtension.class)
-public class CoffeeMachineTest {
 
-    @Mock
-    private Grinder grinder;
-    @Mock
-    private MilkProvider milkProvider;
-    @Mock
-    private CoffeeReceipes receipes;
+@ExtendWith(MockitoExtension.class) public class CoffeeMachineTest {
+
+    @Mock private Grinder grinder;
+    @Mock private MilkProvider milkProvider;
+    @Mock private CoffeeReceipes receipes;
 
     private CoffeeMachine coffeeMachine;
 
-    @BeforeEach
-    void init() {
+    @BeforeEach void init() {
         coffeeMachine = new CoffeeMachine(grinder, milkProvider, receipes);
     }
 
-    
+    @Test void StandardExpressoCoffeShouldntBeTheSameAsRandomlyGenerated() {
 
-    @Test
-    public void itCompiles() {
-        assertThat(true, equalTo(true));
+        CoffeeSize standard = CoffeeSize.STANDARD;
+        CoffeType espresso = CoffeType.ESPRESSO;
+
+        CoffeOrder coffeOrder = CoffeOrder.builder().withSize(standard).withType(espresso).build();
+
+        Coffee result = coffeeMachine.make(coffeOrder);
+
+        Coffee expected = new Coffee();
+        expected.setMilkAmout(1);
+        expected.setCoffeeWeigthGr(1d);
+        expected.setWaterAmount(10);
+
+        Assertions.assertNotEquals(result, expected);
+
     }
 
+
+    @Test public void itCompiles() {
+        assertThat(true, Matchers.equalTo(true));
+    }
 }
